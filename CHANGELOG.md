78df624807639465048e4b8a9cccf014c4f675ca
Tuba Hashmi <hashmiatna@gmail.com>
2023-09-06 08:44:34 +0500

    added resource for users table

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -38,6 +38,29 @@ Maintainers:
 - [x] `apiserver/extensions.py` added for extension(s) registration.
 - [x] `apiserver/manage.py` added to support CLI management of Flask App
 - [x] `apiserver/run.py` added to define entry point.
+<<<<<<< Updated upstream
+=======
+- [x] `apiserver/commons/constants.py` added to define constants of the system.
+- [x] `apiserver/commons/helpers.py` added to define decorators.
+- [x] `apiserver/commons/utilities.py` added to define utilities functions.
+- [x] `apiserver/commons/logging.py` added to define logger.
+- [x] Merged branch `apiserver-setup` [PR#2](https://github.com/tubahashmi/task_manager_api/pull/2).
+- [x] Added following files to baseline apiserver:
+    `apiserver/api/__init__.py`
+    `apiserver/api/models/__init__.py`
+    `apiserver/api/resources/__init__.py`
+    `apiserver/api/schemas/__init__.py`
+    `apiserver/api/views.py`
+- [x] Merged branch `apiserver-setup` [PR#3](https://github.com/tubahashmi/task_manager_api/pull/3)
+- [x] Defined 'Role' for table `role` in the db. Added following files:
+    `apiserver/api/models/roles.py`
+    `apiserver/api/schemas/roles.py`
+- [x] Updated following files:
+    `apiserver/api/models/__init__.py`
+    `apiserver/api/schemas/__init__.py`
+- [x] Added migration for `roles`: `apiserver/migrations/versions/86b4af4e598e_.py`.
+- [x] Added `apiserver/data_management.py` to populate roles: `admin` and `user` in table `roles`.
+- [x] Merged branch `api-roles-resource` [PR#4](https://github.com/tubahashmi/task_manager_api/pull/4)
 - [x] `CHANGELOG.md` updated.
 
 ---
---
+++ apiserver/api/models/users.py
@@ -0,0 +1,64 @@
+#!./venv/bin/python
+# -*- coding: utf-8 -*-
+
+"""Define class for user record."""
+
+# pylint: disable=E1101
+
+import uuid
+from datetime import datetime
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from apiserver.extensions import db
+
+
+class User(db.Model):
+    """
+    Represents a user in the system.
+
+    Attributes:
+        id (str): A unique UUID for the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+        email (str): The email address of the user (unique).
+        password (str): Hashed password of the user.
+        role_id (int): Foreign key to associate a user with a role.
+        created_at (datetime): Timestamp of when the user record was created.
+
+    Relationships:
+        role (Role): Defines the many-to-one relationship between User and Role.
+    """
+    __tablename__ = 'users'
+
+    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()), unique=True, nullable=False)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Define the many-to-one relationship between User and Role
+    role = db.relationship('Role', foreign_keys=[role_id], back_populates='users')
+
+    def set_password(self, password):
+        """
+        Hash and set the user's password.
+
+        Args:
+            password (str): The plain text password to hash and store.
+        """
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        """
+        Check if the provided password matches the stored hashed password.
+
+        Args:
+            password (str): The plain text password to check.
+
+        Returns:
+            bool: True if the password matches, False otherwise.
+        """
+        return check_password_hash(self.password, password)
